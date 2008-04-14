bebf3376a579317aa75d7b2da106c06d21c4fdae
Eric Tavela <eric.tavela2@nih.gov>
2008-04-14 12:07:56 +0000

    Updated script for POJO generation.

--- analysis_and_design/models/scripts/create_pojos_from_model.js
+++ analysis_and_design/models/scripts/create_pojos_from_model.js
@@ -5,6 +5,8 @@ var GENERATED_JAVA_DIR = "c:\\temp\\generated_java";
 var wshShell = WScript.CreateObject("WScript.Shell");
 var repository = WScript.CreateObject("EA.Repository");
 var fso = WScript.CreateObject("Scripting.FileSystemObject");
+mkdirs(GENERATED_JAVA_DIR);
+var log = fso.CreateTextFile(GENERATED_JAVA_DIR + "\\log.txt");
 
 main();
 
@@ -36,7 +38,10 @@ function writePackage(package) {
 
 function writeClasses(package) {
     for (var i = 0; i < package.Elements.Count; i++) {
-        writeClass(package, package.Elements.GetAt(i));
+        var element = package.Elements.GetAt(i);
+        if (element.Type == "Class") {
+            writeClass(package, element);
+        }
     }
 }
 
@@ -52,40 +57,126 @@ function writeClass(package, element) {
 }
 
 function writePackageStatement(javaFile, package) {
-    outputFile.WriteLine("package " + package.Name + ";");
+    javaFile.WriteLine("package " + package.Name + ";");
 }
 
 function writeImports(javaFile, element) {
-    outputFile.WriteLine();
+    javaFile.WriteLine();
 }
 
 function writeClassHeading(javaFile, element) {
-    outputFile.WriteLine("/**");
-    outputFile.WriteLine(" * " + element.Notes);
-    outputFile.WriteLine(" */");
-    outputFile.WriteLine("public class " + element.Name + " {");
+    var documentation;
+    if (!hasTaggedValue(DOCUMENTATION_TAG, element)) {
+        log.WriteLine("WARN: " + element.Name + " has no documentation tagged value");
+        documentation = element.Notes;
+    } else {
+        documentation = getTaggedValue(DOCUMENTATION_TAG, element);
+    }
+    javaFile.WriteLine("/**");
+    javaFile.WriteLine(" * " + documentation);
+    javaFile.WriteLine(" */");
+    javaFile.Write("public class " + element.Name);
+    if (element.BaseClasses.Count > 0) {
+        javaFile.Write(" extends " + element.BaseClasses.GetAt(0).Name);
+    }
+    javaFile.Write(" {");
+    javaFile.WriteLine();
 }
 
 function writeAttributes(javaFile, element) {
-    outputFile.WriteLine();
+    javaFile.WriteLine();
+    for (var i = 0; i < element.Attributes.Count; i++) {
+        var attribute = element.Attributes.GetAt(i);
+        javaFile.WriteLine("    private " + attribute.Type + " " + attribute.Name + ";");
+    }
 }
 
 function writeAssociations(javaFile, element) {
-    outputFile.WriteLine();
+    for (var i = 0; i < element.Connectors.Count; i++) {
+        var connector = element.Connectors.GetAt(i);
+        if (isAssociation(connector) && isOtherEndNavigable(element, connector)) {
+            writeAssociation(javaFile, element, connector);
+        }
+    }
+}
+
+function writeAssociation(javaFile, element, connector) {
+    var otherEnd = getOtherEnd(element, connector);
+    var associatedClass = getOtherEndClass(element, connector);
+    javaFile.WriteLine("    private " + associatedClass.Name + " " + otherEnd.Role + ";");
+}
+
+function isAssociation(connector) {
+    return "Association" == connector.Type || "Aggregation" == connector.Type;
+}
+
+function isOtherEndNavigable(element, connector) {
+    //return getOtherEnd(element, connector).isNavigable;
+    return true;
+}
+
+function getOtherEnd(element, connector) {
+    if (isClientEnd(element, connector)) {
+        return element.SupplierEnd;
+    } else {
+        return element.ClientEnd;
+    }
+}
+
+function getOtherEndClass(element, connector) {
+    if (isClientEnd(element, connector)) {
+        return repository.GetElementByID(element.SupplierID);
+    } else {
+        return repository.GetElementByID(element.ClientID);
+    }
+}
+
+function isClientEnd(element, connector) {
+    return connector.ClientID == element.ElementID;
 }
 
 function writeGettersAndSetters(javaFile, element) {
-    outputFile.WriteLine();
+    javaFile.WriteLine();
+    for (var i = 0; i < element.Attributes.Count; i++) {
+        var attribute = element.Attributes.GetAt(i);
+        var documentation;
+        if (!hasTaggedValue(DESCRIPTION_TAG, attribute)) {
+            log.WriteLine("WARN: " + element.Name + "." + attribute.name + " has no description tagged value");
+            documentation = attribute.Notes;
+        } else {
+            documentation = getTaggedValue(DESCRIPTION_TAG, attribute);
+        }
+        javaFile.WriteLine("    /**");
+        javaFile.WriteLine("     * Getter for " + attribute.Name + ": " + documentation);
+        javaFile.WriteLine("     */");
+        javaFile.WriteLine("    public " + attribute.Type + " get" + getInitialUpper(attribute.Name) + "() {");
+        javaFile.WriteLine("        return " + attribute.Name + ";");
+        javaFile.WriteLine("    }");
+        javaFile.WriteLine();
+        javaFile.WriteLine("    /**");
+        javaFile.WriteLine("     * Setter for " + attribute.Name + ": " + documentation);
+        javaFile.WriteLine("     */");
+        javaFile.WriteLine("    public void set" + getInitialUpper(attribute.Name) + "(" + attribute.Type + " " + attribute.Name + ") {");
+        javaFile.WriteLine("        this." + attribute.Name + " = " + attribute.Name + ";");
+        javaFile.WriteLine("    }");
+        javaFile.WriteLine();
+    }
+}
+
+function getInitialUpper(value) {
+    return value.charAt(0).toUpperCase() + value.substring(1, value.length);
 }
 
 function writeClassEnd(javaFile) {
-    outputFile.WriteLine("}");
+    javaFile.WriteLine("}");
 }
 
 function createJavaFile(package, element) {
     var directoryPath = GENERATED_JAVA_DIR + "\\" + getDirectoryPath(package);
     mkdirs(directoryPath);
-    return fso.CreateTextFile(directoryPath + "\\" + element.Name + ".java", true);
+    var filename = directoryPath + "\\" + element.Name + ".java";
+    log.WriteLine("Creating java file " + filename);
+    return fso.CreateTextFile(filename, true);
 }
 
 function mkdirs(path) {
@@ -100,19 +191,27 @@ function mkdirs(path) {
 }
 
 function getDirectoryPath(package) {
-    return package.Name.replace("\.", "\\\\");
+    return package.Name.replace(/\./g, "\\");
 }
 
-function writeClassNotes(element) {
-    if (hasTaggedValue(DOCUMENTATION_TAG, element)) {
-        element.Notes = getTaggedValue(DOCUMENTATION_TAG, element);
+function hasTaggedValue(tagName, parent) {
+    for (var i = 0; i < parent.TaggedValues.Count; i++) {
+        var tag = parent.TaggedValues.GetAt(i);
+        if (tag.Name == tagName) {
+            return true;
+        }
     }
+    return false;
 }
 
-function writeAttribute(attribute) {
-    if (hasTaggedValue(DESCRIPTION_TAG, attribute)) {
-        attribute.Notes = getTaggedValue(DESCRIPTION_TAG, attribute);
+function getTaggedValue(tagName, parent) {
+    for (var i = 0; i < parent.TaggedValues.Count; i++) {
+        var tag = parent.TaggedValues.GetAt(i);
+        if (tag.name == tagName) {
+            return tag.Value;
+        }
     }
+    return "";
 }
 
 function alert(message) {
